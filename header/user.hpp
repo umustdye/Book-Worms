54dc28c5de9c5b7481e66efdea5faa55f42ced08
ChristopherCE <christopheralexman@gmail.com>
2021-02-23 22:45:14 -0800

    Add getter/setter functions for user std::string variables
    * Add variables for user class for account and logging in
    * Add getter / setter functions to interact with private variables

--- header/user.hpp
+++ header/user.hpp
@@ -1,8 +1,27 @@
 #ifndef __USER_HPP__
 #define __USER_HPP__
 
-Class User {
+#include <string>
+#include <vector>
+#include <utility>
 
+class User {
+private:
+    std::string userName;
+    std::string password;
+    std::string firstName;
+    std::string lastName;
+    std::vector<std::pair<int, int>> userItems;
+
+public:
+    void setUserName(std::string);
+    void setPassword(std::string);
+    void setFirstName(std::string);
+    void setLastName(std::string);
+    std::string getUserName();
+    std::string getPassword();
+    std::string getFirstName();
+    std::string getLastName();
 };
 
 #endif //__USER_HPP__
--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,33 @@
 #include "../header/user.hpp"
+
+void User::setUserName(std::string u) {
+    this->userName = u;
+}
+
+void User::setPassword(std::string p) {
+    this->password = p;
+}
+
+void User::setFirstName(std::string f) {
+    this->firstName = f;
+}
+
+void User::setLastName(std::string l) {
+    this->lastName = l;
+}
+
+std::string User::getUserName() {
+    return this->userName;
+}
+
+std::string User::getPassword() {
+    return this->password;
+}
+
+std::string User::getFirstName() {
+    return this->firstName;
+}
+
+std::string User::getLastName() {
+    return this->lastName;
+}
