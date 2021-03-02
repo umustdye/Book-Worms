850ca88f29baee71abc06b2346f9f8119d28d464
ChristopherCE <christopheralexman@gmail.com>
2021-03-02 08:34:00 -0800

    Add start of library class
    * Library class will connect the composite and factory patterns,
      or the item and account classes
    * Need to set up database and Qt pages first, then the rest of the functions

--- header/library.hpp
+++ header/library.hpp
@@ -1,8 +1,19 @@
 #ifndef __LIBRARY_HPP__
 #define __LIBRARY_HPP__
 
-class Library {
+#include "account.hpp"
+#include "item.hpp"
 
+class Library {
+private:
+    Account* acnt;
+    LibraryItemComponent* item;
+
+public:
+    Library(Account* a, LibraryItemComponent* i);
+    ~Library();
+    void checkOutItem();
+    void returnItem();
 };
 
 #endif //__LIBRARY_HPP__
---
+++ src/library.cpp
@@ -0,0 +1,13 @@
+#include "library.hpp"
+
+Library::Library(Account *a, LibraryItemComponent *i) : acnt(a), item(i) {}
+
+Library::~Library() {}
+
+void Library::checkOutItem() {
+
+}
+
+void Library::returnItem() {
+
+}
