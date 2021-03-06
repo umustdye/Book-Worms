a8aeaa00b7c879eb61db66976d9eee9b7633f199
SebastianHall69 <sebastianhall13@gmail.com>
2021-03-05 21:42:22 -0800

    Added QDialog class to create dvd from user input

---
+++ header/dvddialog.hpp
@@ -0,0 +1,30 @@
+#ifndef DVDDIALOG_HPP
+#define DVDDIALOG_HPP
+
+#include <QDialog>
+#include "dvd.hpp"
+
+namespace Ui {
+class DvdDialog;
+}
+
+class DvdDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DvdDialog(QWidget *parent = nullptr);
+    ~DvdDialog();
+    Dvd* create(int id = -1);
+
+private:
+    Ui::DvdDialog *ui;
+    Dvd *dvd;
+    int id;
+
+public slots:
+    void accept() override;
+    void reject() override;
+};
+
+#endif // DVDDIALOG_HPP
